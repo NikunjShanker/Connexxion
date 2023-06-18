ba471f716cb1148c0820b5d33e4af9fbf24607d6
Nikunj Shanker <nikunj.shanker@gmail.com>
2023-06-17 20:57:27 -0700

    Project Created and Dots Mouse Interaction Started

---
+++ Connexxion/Assets/Scripts/DotScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotScript : MonoBehaviour
+{
+    private string name;
+    private string type;
+
+    private void Start()
+    {
+        name = this.transform.gameObject.name;
+        type = "white circle";
+    }
+
+    private void OnMouseDrag()
+    {
+        GameplayControllerScript.instance.DotSelected(0, type, name);
+    }
+}
---
+++ Connexxion/Assets/Scripts/GameplayControllerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayControllerScript : MonoBehaviour
+{
+    public static GameplayControllerScript instance;
+    public GameObject[] dots;
+
+    private Camera cam;
+    
+    private void Start()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+        GetAllDots();
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void DotSelected(int order, string type, string name)
+    {
+        Debug.Log(name);
+    }
+
+    private void GetAllDots()
+    {
+        Transform canvas = GameObject.Find("Dots Canvas").transform;
+        dots = new GameObject[canvas.childCount];
+        for (int i = 0; i < dots.Length; i++)
+        {
+            dots[i] = canvas.GetChild(i).gameObject;
+        }
+    }
+}
